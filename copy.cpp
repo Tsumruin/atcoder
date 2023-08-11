e02420c6af7b5e64bb4e1ddd9f8001b6495dc0e8
pensuke <sho.pensuke@gmail.com>
2023-08-11 19:45:13 +0900

    update

---
+++ copy.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <iomanip> 
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+
+    int N; cin >> N;
+
+    vector<int> A(N);
+    for(int i=0; i<N; ++i){
+        cin >> A.at(i);
+    }
+
+    return 0;
+}
