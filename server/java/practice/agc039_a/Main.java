ef541248cb8fa63d5091e8557068808eed602bfc
pensuke <sho.pensuke@gmail.com>
2023-11-20 21:28:12 +0900

    add agc039_a

---
+++ server/java/practice/agc039_a/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    private static void solution(){
+        try (Scanner sc = new Scanner(System.in)) {
+            String S = sc.next();
+            long K = Long.parseLong(sc.next());
+            
+
+        }
+    }
+}
