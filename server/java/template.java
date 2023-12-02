f29b4e3b6e000363c5c44267a5c9fd61b2d3ed9a
pensuke <sho.pensuke@gmail.com>
2023-12-02 19:36:33 +0900

    fix

--- server/java/template.java
+++ server/java/template.java
@@ -1,4 +1,9 @@
-import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
