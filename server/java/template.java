5b8073f7973ad54dc1936c989b3416cc2999c95b
pensuke <sho.pensuke@gmail.com>
2023-12-02 17:48:17 +0900

    rename

--- server/java/template.java
+++ server/java/template.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         solution();
     }
@@ -14,5 +14,50 @@ public class Main {
                 A[i] = Integer.parseInt(sc.next());
             }
         }
+    }
+}
+
+
+class TestCaseFileGenerator {
+    public static void main(String[] args) {
+        generateTestCaseFile("data.txt");
+    }
+
+    private static void generateTestCaseFile(String filename) {
+        Random random = new Random();
+
+        try(Scanner sc = new Scanner(System.in)){
+            int N = Integer.parseInt(sc.next()); // 例として N = 10 とします
+            int M = Integer.parseInt(sc.next()); // 例として M = 8 とします
+
+            try (FileWriter writer = new FileWriter(filename)) {
+                writer.write(N + " " + M + "\n");
+
+                List<Integer> listA = generateRandomNumbers(N, random);
+                writer.write(listToString(listA) + "\n");
+
+                List<Integer> listB = generateRandomNumbers(M, random);
+                writer.write(listToString(listB) + "\n");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static List<Integer> generateRandomNumbers(int size, Random random) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            numbers.add(random.nextInt(1000));
+        }
+        return numbers;
+    }
+
+    private static String listToString(List<Integer> list) {
+        StringBuilder builder = new StringBuilder();
+        for (Integer num : list) {
+            builder.append(num).append(" ");
+        }
+        return builder.toString().trim();
     }
 }
